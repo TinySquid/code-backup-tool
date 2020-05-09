40a7774c8805bd7ebb5bf6a95697fc5c96c7f7c1
Michael Nunes <tinysquid.git@gmail.com>
2020-05-08 19:37:55 -0700

    Add some error handling to JSON parsing

--- main.py
+++ main.py
@@ -25,6 +25,10 @@
 
     Commence the backup process by recursively going through dirs,
     copying files over as necessary.
+
+    Path doesn't exist in backup folder -> create dirs + intermediate dirs and copy over file
+    Path exists but file doesn't -> Copy file over
+    Path and file exists -> Overwrite if older
 """
 
 import os  # For path stuff
@@ -33,7 +37,7 @@ import shutil  # For file copy / overwrite / metadata
 import json  # For parsing config
 
 
-def print_usage(error_message):
+def print_usage(error_message=None):
     """
     Prints optional error message and usage statement
     """
@@ -41,7 +45,7 @@ def print_usage(error_message):
         print(error_message)
 
     print(
-        "usage: python main.py [help] [config_file]\n[help]: Prints usage statement\n[config_file]: Path to custom config"
+        "usage: python main.py [help] [config_file.json]\n[help]: Prints usage statement\n[config_file]: Path to custom config"
     )
     # LEAVE!!!
     exit()
@@ -57,10 +61,14 @@ def load_config(file):
         with open(file) as config_file:
             # Returning from inside a "with" statement will close file
             # automatically still.
-            return json.load(config_file)
+            try:
+                config = json.load(config_file)
+            except json.JSONDecodeError:
+                print_usage("Not a valid JSON file")
+
+            return config
     else:
-        print(f"Error: Config file {file} does not exist.")
-        exit(1)
+        print_usage(f"Config file {file} does not exist.")
 
 
 def parse_args(args):
@@ -74,7 +82,7 @@ def parse_args(args):
     if args_len == 1:
         return load_config(default_config_path)
     elif args_len == 2:
-        if args[1] == "-h":
+        if args[1] == "help":
             print_usage()
         else:
             return load_config(args[1])
@@ -84,103 +92,105 @@ def parse_args(args):
 
 # * TODO - Allow for full commandline only operation as a second option (instead of loading from a config file)
 
-if __name__ == "__main__":
-    # Parse args and return loaded config
-    config = parse_args(sys.argv)
+# Parse args and return loaded config
+config = parse_args(sys.argv)
 
-    # # Pull config into vars
-    # backup_from_folder = config["backup-from-folder"]
-    # backup_to_folder = config["backup-to-folder"]
-    # folder_exclusions = config["folder-exclusions"]
-    # filetype_exclusions = config["filetype-exclusions"]
-    # filename_exclusions = config["filename-exclusions"]
+for key in config:
+    print(key, config[key])
 
-    # # Bools to toggle exclusion functionality
-    # enable_folder_exclusions = True if len(folder_exclusions) > 0 else False
-    # enable_filename_exclusions = True if len(filename_exclusions) > 0 else False
-    # enable_filetype_exclusions = True if len(filetype_exclusions) > 0 else False
+# # Pull config into vars
+# backup_from_folder = config["backup-from-folder"]
+# backup_to_folder = config["backup-to-folder"]
+# folder_exclusions = config["folder-exclusions"]
+# filetype_exclusions = config["filetype-exclusions"]
+# filename_exclusions = config["filename-exclusions"]
 
-    # print(config)
-    # print("Config loaded!")
-    # print("Starting...")
+# # Bools to toggle exclusion functionality
+# enable_folder_exclusions = True if len(folder_exclusions) > 0 else False
+# enable_filename_exclusions = True if len(filename_exclusions) > 0 else False
+# enable_filetype_exclusions = True if len(filetype_exclusions) > 0 else False
 
-    # # List to store filtered paths + filenames
-    # # foo/bar.txt
-    # paths = []
+# print(config)
+# print("Config loaded!")
+# print("Starting...")
 
-    # # Traverse all files and folders that pass the critera (exclusions)
-    # for path, subdirs, files in os.walk(backup_from_folder, topdown=True):
-    #     # * Filter by folder name (in-place)
-    #     if enable_folder_exclusions:
-    #         subdirs[:] = [d for d in subdirs if d not in folder_exclusions]
+# # List to store filtered paths + filenames
+# # foo/bar.txt
+# paths = []
 
-    #     # * iterate over all files in this directory
-    #     for file in files:
-    #         # Get file extension
-    #         file_type = os.path.splitext(file)[1]
+# # Traverse all files and folders that pass the critera (exclusions)
+# for path, subdirs, files in os.walk(backup_from_folder, topdown=True):
+#     # * Filter by folder name (in-place)
+#     if enable_folder_exclusions:
+#         subdirs[:] = [d for d in subdirs if d not in folder_exclusions]
 
-    #         if enable_filename_exclusions:
-    #             # Does filename pass exclusion filter?
-    #             if file not in filename_exclusions:
-    #                 # * Filter by filename & filetype
-    #                 if enable_filetype_exclusions:
-    #                     # Does filetype pass exclusion filter?
-    #                     if file_type not in filetype_exclusions:
-    #                         paths.append(os.path.join(path, file))
-    #                 else:
-    #                     # * Filter by just filename
-    #                     paths.append(os.path.join(path, file))
-    #         elif enable_filetype_exclusions:
-    #             # Does filetype pass exclusion filter?
-    #             if file_type not in filetype_exclusions:
-    #                 # * Filter by just filetype
-    #                 paths.append(os.path.join(path, file))
-    #         else:
-    #             # * No filters besides folder name
-    #             paths.append(os.path.join(path, file))
+#     # * iterate over all files in this directory
+#     for file in files:
+#         # Get file extension
+#         file_type = os.path.splitext(file)[1]
 
-    # from_paths = paths
+#         if enable_filename_exclusions:
+#             # Does filename pass exclusion filter?
+#             if file not in filename_exclusions:
+#                 # * Filter by filename & filetype
+#                 if enable_filetype_exclusions:
+#                     # Does filetype pass exclusion filter?
+#                     if file_type not in filetype_exclusions:
+#                         paths.append(os.path.join(path, file))
+#                 else:
+#                     # * Filter by just filename
+#                     paths.append(os.path.join(path, file))
+#         elif enable_filetype_exclusions:
+#             # Does filetype pass exclusion filter?
+#             if file_type not in filetype_exclusions:
+#                 # * Filter by just filetype
+#                 paths.append(os.path.join(path, file))
+#         else:
+#             # * No filters besides folder name
+#             paths.append(os.path.join(path, file))
 
-    # to_paths = []
+# from_paths = paths
 
-    # for path in from_paths:
-    #     to_paths.append(
-    #         os.path.join(backup_to_folder, os.path.relpath(path, backup_from_folder))
-    #     )
+# to_paths = []
 
-    # for i, full_path in enumerate(to_paths):
-    #     print(f"{i}: {full_path} | {to_paths[i]}")
-    #     # if os.path.exists(os.path.dirname(full_path)):
-    #     #     # * Path exists, but does the file?
-    #     #     if os.path.exists(full_path):
-    #     #         # * Overwrite file if newer than backup
-    #     #         pass
-    #     #     else:
-    #     #         # * Path exists, but file doesn't. Copy over file
-    #     #         pass
-    #     # else:
-    #     #     # * Path doesn't exist, so neither does the file. Create dirs and copy over file
-    #     #     path = os.path.dirname(full_path)
+# for path in from_paths:
+#     to_paths.append(
+#         os.path.join(backup_to_folder, os.path.relpath(path, backup_from_folder))
+#     )
 
-    #     #     try:
-    #     #         os.makedirs(path)
-    #     #     except FileExistsError:
-    #     #         print("Path already exists")
+# for i, full_path in enumerate(to_paths):
+#     print(f"{i}: {full_path} | {to_paths[i]}")
+#     # if os.path.exists(os.path.dirname(full_path)):
+#     #     # * Path exists, but does the file?
+#     #     if os.path.exists(full_path):
+#     #         # * Overwrite file if newer than backup
+#     #         pass
+#     #     else:
+#     #         # * Path exists, but file doesn't. Copy over file
+#     #         pass
+#     # else:
+#     #     # * Path doesn't exist, so neither does the file. Create dirs and copy over file
+#     #     path = os.path.dirname(full_path)
 
-    #     #     # Copy over file
-    #     #     shutil.copy2()
+#     #     try:
+#     #         os.makedirs(path)
+#     #     except FileExistsError:
+#     #         print("Path already exists")
 
-    #     # if os.path.exists(path):
-    #     #     # File already exists, check modified date and overwrite if newer
-    #     # else:
-    #     #     # Either the path doesn't exist (dirs) or the file doesn't exist
-    #     #     if os.path.exists(os.path.dirname(path)):
-    #     #         #* Path exits
-    #     #     else:
-    #     #         # * Path doesn't exist, and therfore neither does the file
-    #     #     pass
-    #     # else:
-    #     #     # File doesn't exist, copy over and build dir + intermediate dirs if needed
-    #     #     # Extract path
+#     #     # Copy over file
+#     #     shutil.copy2()
 
-    # print("Done!")
+#     # if os.path.exists(path):
+#     #     # File already exists, check modified date and overwrite if newer
+#     # else:
+#     #     # Either the path doesn't exist (dirs) or the file doesn't exist
+#     #     if os.path.exists(os.path.dirname(path)):
+#     #         #* Path exits
+#     #     else:
+#     #         # * Path doesn't exist, and therfore neither does the file
+#     #     pass
+#     # else:
+#     #     # File doesn't exist, copy over and build dir + intermediate dirs if needed
+#     #     # Extract path
+
+# print("Done!")
